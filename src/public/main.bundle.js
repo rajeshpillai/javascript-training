9c716ac09b2019563f98b7db3e996f1aea31ff66
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-03-22 21:33:28 +0530

    Cleanup

--- src/public/main.bundle.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _proxy = require('./advanced/01-proxy/proxy');
-
-var _proxy2 = _interopRequireDefault(_proxy);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-console.log(_proxy2.default);
