25028f1f37242d5e7e31f232404bef11a0270f98
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-03-22 21:58:31 +0530

    Proxy setter method

--- advanced/01-proxy/proxy.js
+++ advanced/01-proxy/proxy.js
@@ -2,7 +2,14 @@ const Handler = {
     get(target, name) {
         console.log(`${target} calling ${name}`);
         return (...args) => target.send(name, args);
-    }
+    },
+
+    set(obj, prop, value) {
+        // The default behavior to store the value
+        console.log(`updating ${prop} to ${value}`);
+        obj[prop] = value;
+        return true;
+      }
 }
 
 export default Handler;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import handler from '../advanced/01-proxy/proxy';
 
-console.log(handler);
+class User {
+    constructor() {
+        this.username = "";
+        this.email = "";
+    }
+
+    get_name() {
+        return this.username;
+    }
+}
+
+let p = new Proxy(new User(), handler);
+p.username = "rajesh";
+p.email = "abc@test.com";
+
+console.log(p.username);
+console.log(p.email);
+
+console.log(p.get_name());
